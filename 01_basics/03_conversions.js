c3bea520d0ddc4efa3efbf935fa26c049df2d4c6
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-29 03:06:22 +0530

    comparison and operations

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,57 @@
+// let score = "33abc";
+
+// // console.log(typeof score);
+
+// // let valueInNumber = Number(score);
+
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
+
+// /**
+//  *  "33" => 33
+//  *  "33abc" => NaN type number
+//  *  true => 1
+//  *  false => 0
+//  *
+//  */
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// /**
+//  * 1 => true, 0 => false
+//  * "" => false, "string" => true
+//  */
+
+// let someNum = 345;
+
+// let stringNum = String(someNum);
+
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// ***************** Operations ***********************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2 + 45);
+// console.log(23 - 5);
+// console.log(2 * 5);
+// console.log(2 ** 5);
+// console.log(2 / 5);
+// console.log(14 % 6);
+
+let str1 = "hello ";
+
+let str2 = "Saurav";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
